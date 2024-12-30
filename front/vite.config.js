81c483ad8befd6693252eee601b168048be571e5
yfidati <fidati15@gmail.com>
2024-12-30 12:57:09 +0100

    Update  vite.config

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    port: process.env.PORT || 4173, // Use Render's port or default to 4173
+    host: '0.0.0.0', // Make the server accessible externally
+  },
+  build: {
+    outDir: 'dist',  // Make sure your output is correctly set
+  }
+};
+
